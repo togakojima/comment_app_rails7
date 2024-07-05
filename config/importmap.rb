9ac1ec577160e216c49c3a01b0ab71850033bfe6
togakojima <togakojima1999@gmail.com>
2024-07-05 18:16:15 +0900

    pinの削除

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,5 +7,3 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "@rails/actioncable", to: "actioncable.esm.js"
 pin_all_from "app/javascript/channels", under: "channels"
-pin "app/javascript/channels/comment_channel.js", to: "channels/comment_channel.js"
-pin "app/javascript/channels/consumer.js", to: "channels/consumer.js"
