9b9c83ba142c85281bfdf33a54466058614dce09
togakojima <togakojima1999@gmail.com>
2024-07-05 18:13:50 +0900

    ./をchannelに変更

--- app/javascript/channels/comment_channel.js
+++ app/javascript/channels/comment_channel.js
@@ -1,46 +1,33 @@
-import consumer from "./consumer"
+import consumer from "channels/consumer"
 
 document.addEventListener('turbo:load', () => {
-  let subscriber;
   if(location.pathname.match(/\/items\/\d/)){
+
+    // 以下を追加
+      consumer.subscriptions.create({
+        channel: "CommentChannel",
+        item_id: location.pathname.match(/\d+/)[0]
+      }, {
     
-    const itemId = location.pathname.match(/\d+/)[0]
-    let subscription = consumer.subscriptions.findAll(subs => subs.identifier === 
-      JSON.stringify({channel: 'CommentChannel', item_id: itemId }))[0]
-  
-    if (subscription){
-      // If subsription already exists, remove it first
-      consumer.subscriptions.remove(subscription)
+        connected() {
+          // Called when the subscription is ready for use on the server
+        },
+    
+        disconnected() {
+          // Called when the subscription has been terminated by the server
+        },
+    
+        received(data) {
+          const html = `
+            <div class="comment">
+              <p class="user-info">${data.user.nickname}： </p>
+              <p>${data.comment.text}</p>
+            </div>`
+          const comments = document.getElementById("comments")
+          comments.insertAdjacentHTML('beforeend', html)
+          const commentForm = document.getElementById("comment-form")
+          commentForm.reset();
+        }
+      })
     }
-  
-    // Then create a subscription to the current page
-    subscriber = consumer.subscriptions.create({
-      channel: "CommentChannel",
-      item_id: itemId
-    },
-    {
-      // ...
-    })
-  
-    subscriber.connected = () => {
-      // Called when the subscription is ready for use on the server
-    }
-  
-    subscriber.disconnected = () => {
-      // Called when the subscription has been terminated by the server
-    }
-  
-    subscriber.received = (data) => {
-      console.log(data)
-      const html = `
-        <div class="comment">
-          <p class="user-info">${data.user.nickname}： </p>
-          <p>${data.comment.text}</p>
-        </div>`
-      const comments = document.getElementById("comments")
-      comments.insertAdjacentHTML('beforeend', html)
-      const commentForm = document.getElementById("comment-form")
-      commentForm.reset();
-    }
-  }
-});
+  });
